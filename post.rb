0519b8dea39098b23511090828e199a53f41929b
Tobin Elavathil <tobin.elavathil@gmail.com>
2016-06-30 08:46:34 -0400

    Parent Id bugs removed, method all formating incomplete

--- post.rb
+++ post.rb
@@ -1,36 +1,31 @@
 require 'nokogiri'
+require './comment.rb'
+require 'pry'
 #require_relative './post.html'
 
 class Post
-
-
-
-  attr_accessor :title, :url, :points, :item_id, :comments
+  attr_accessor :title, :url, :points, :item_id, :all_comments
 
   def initialize
-    self.doc = Nokogiri::HTML(File.open('./post.html'))
-    self.title = self.doc.css('span.score').map {|span| span.inner_text}
+    doc = Nokogiri::HTML(File.open('./post.html'))
+    self.title = doc.css('span.score').map {|span| span.inner_text}
     self.url = 'https://news.ycombinator.com/item?id=7663775'
-    self.points = self.doc.css('span.score').map {|span| span.inner_text}
+    self.points = doc.css('span.score').map {|span| span.inner_text}
     self.item_id = self.url.split('?id=')[1]
-    self.comments = []
-  end
-
-  #Post#comments returns all the comments associated with a particular post.
-  def comments
+    self.all_comments = []
 
     #loop over all the comments
     #store the parent_id and last id on each iteration
     parents=[-1]
     previous = 0
     last_id = 0
-    comment_list = doc.css('tr.athing.comtr').each {|tr|
+    doc.css('tr.athing.comtr').each {|tr|
       #create a new comment
       c = Comment.new
       #get id
       c.id = tr['id']
       #get authour
-      c.authour = tr.css('a.hnuser').map {|link| link.inner_text}
+      c.author = tr.css('a.hnuser').map {|link| link.inner_text}
       #get days_ago
       c.days_ago = tr.css('span.age > a').map {|link| link.inner_text}
       #get text
@@ -38,28 +33,34 @@ class Post
 
       #create parent_id in a stack
       #store indent
-      current = tr.css('td.ind > img')[0]['width']
+      #binding.pry
+      current = tr.css('td.ind > img')[0]['width'].to_i
       if current > previous
         parents << last_id
       elsif current < previous
-        ((current-previous)/40).times {parents.shift}
+        parents.pop((previous-current)/40)
       end
       c.parent_id = parents.last
       previous = current
       last_id = c.id
 
       #add comment to the comment list
-      #add_comment(c)
+      #binding.pry
+      add_comment(c)
+    }
+  end
 
+  #Post#comments returns all the comments associated with a particular post.
+  def comments
     #print comments
-    comments_list
+    self.all_comments
   end
 
   #Post#add_comment takes a Comment object as its input and adds it to the comment list.
   def add_comment(comment)
-    self.comments << comment
+    self.all_comments << comment
   end
 end
 
 p = Post.new
-puts p
+puts p.comments
